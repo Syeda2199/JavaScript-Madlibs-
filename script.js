ea4982bb617f038c0aec530728c5c5220860ea55
SyedaAkther <syedaakther120@gmail.com>
2024-07-31 19:10:41 +0000

    description of your changes

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+let food = "shawarma"
+let drink = "fruit punch"
+let wallet = 100
+let designer = "Khaadi"
+let car = "ferrari"
+let adjective = "extraordinary"
+let noun = "Rabbit"
+let verb = "help"
+let number = 14
+let calculatedNumber = (4*8)
+let isExtraordinary = true
+let username = prompt("What is your name?")
+let story = `Once upon a time, there was a ${adjective} ${noun} that loved to ${verb}. 
+It had ${number} magical powers, which it used to help the world.
+One day, ${username} discovered that the ${noun} could actually ${verb} with ${calculatedNumber} times the speed of light.
+Wasn't that extraordinary? $(isExtraordinary)`;
 
+ // Insert the story into the document 
+ document.getElementById("story").innerText = story;
